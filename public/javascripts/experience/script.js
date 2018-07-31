fb1d9eb8ae4e9a315c6e35f12a71de04f19a4d1c
Eric Tiedeken <eric.tiedeken@gmail.com>
2018-07-31 14:48:09 -0500

    added experience images.

--- public/javascripts/experience/script.js
+++ public/javascripts/experience/script.js
@@ -4,6 +4,15 @@
             this.etiedeken = etiedeken;
         }
         fulfiller(ajax) {
+            let experienceElement = document.getElementById('experience');
+
+            let container = this.etiedeken.element('div', ['container', 'no-bg'], []);
+
+            ajax.experience.reverse();
+            for (let i = ajax.experience.length; i > 0; --i)
+                container.appendChild(this.etiedeken.image([], [], ajax.experience[i-1].image, ajax.experience[i-1].company));
+
+            experienceElement.appendChild(container);
         }
 
         ajax(ajax) {
