e2adb02f3faad123cbfa63513b6fc3352e43d4b0
Eric Tiedeken <eric.tiedeken@gmail.com>
2018-07-31 12:43:33 -0500

    organized files

--- routes/index.js
+++ routes/index.js
@@ -3,12 +3,12 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Game Designer and Developer | Web Designer and Developer | Portfolio | Eric Tiedeken'});
+  res.render('es6/index', { title: 'Game Designer and Developer | Web Designer and Developer | Portfolio | Eric Tiedeken'});
 });
 
 /* GET home page AMP. */
 router.get('/amp/', function(req, res, next) {
-  res.render('amp_index', { title: 'Hello, AMPs'});
+  res.render('amp/index', { title: 'Hello, AMPs'});
 });
 
 module.exports = router;
