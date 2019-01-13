55df3b84310bf46da28078b2307939ea97be61b9
Speed Force <speedforce@The-Flash.local>
2019-01-13 16:30:47 -0600

    removed not required code

--- project/routes/page.js
+++ project/routes/page.js
@@ -12,24 +12,24 @@ router.get('/:page', function(req, res, next) {
     age: age
   };
   switch (req.params.page) {
-    case 'es6':
-      params.title = 'ES6 Version | Game Designer and Developer | Web Designer and Developer | Portfolio | Eric Tiedeken';
-      res.render('es6/index', params);
-      break;
-    case 'angularjs':
-      params.title = 'AngularJS Version | Game Designer and Developer | Web Designer and Developer | Portfolio | Eric Tiedeken';
-      res.render('angularjs/index', params);
-      break;
+    // case 'es6':
+    //   params.title = 'ES6 Version | Game Designer and Developer | Web Designer and Developer | Portfolio | Eric Tiedeken';
+    //   res.render('es6/index', params);
+    //   break;
+    // case 'angularjs':
+    //   params.title = 'AngularJS Version | Game Designer and Developer | Web Designer and Developer | Portfolio | Eric Tiedeken';
+    //   res.render('angularjs/index', params);
+    //   break;
     case 'pwa':
       params.title = 'PWA | Game Designer and Developer | Web Designer and Developer | Portfolio | Eric Tiedeken';
       res.render('pwa/index', params);
       break;
-    case 'amp':
-      res.header('Access-Control-Allow-Origin', '*');
-      res.header('Access-Control-Expose-Headers', 'AMP-Access-Control-Allow-Source-Origin');
-      res.header('AMP-Access-Control-Allow-Source-Origin', 'http://localhost:3000');
-      res.render('amp/index', { title: 'AMP Version | Game Designer and Developer | Web Designer and Developer | Portfolio | Eric Tiedeken'});
-      break;
+    // case 'amp':
+    //   res.header('Access-Control-Allow-Origin', '*');
+    //   res.header('Access-Control-Expose-Headers', 'AMP-Access-Control-Allow-Source-Origin');
+    //   res.header('AMP-Access-Control-Allow-Source-Origin', 'http://localhost:3000');
+    //   res.render('amp/index', { title: 'AMP Version | Game Designer and Developer | Web Designer and Developer | Portfolio | Eric Tiedeken'});
+    //   break;
     default:
       next();
   }
