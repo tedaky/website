29ceb8707bf14e256a240494d7804301d458f8b6
Speed Force <speedforce@The-Flash.local>
2019-01-13 16:24:08 -0600

    birthday test

--- project/extras/birthday.spec.js
+++ project/extras/birthday.spec.js
@@ -8,15 +8,15 @@ describe('loading birthday', function() {
   beforeEach(function() {
     birthday = require('./birthday');
     today = new Date();
-    birthdayBefore = today.setDate(today.getDate() - 5);
-    birthdayAfter = today.setDate(today.getDate() + 5);
+    birthdayBefore = today.setDate(today.getDate() - 550);
+    birthdayAfter = today.setDate(today.getDate() - 364);
   });
   it('responds to Birthday before Today\'s date', function birthdayBeforeNow() {
     // Todo: correct the test
-    assert.equal(birthday(birthdayBefore), 0);
+    assert.equal(birthday(birthdayBefore), 1);
   });
   it('responds to Birthday after Today\'s date', function birthdayBeforeNow() {
     // Todo: correct the test
-    assert.equal(birthday(birthdayAfter), 0);
+    assert.equal(birthday(birthdayAfter), 2);
   });
 });
